b721c720ede00cbbcd556ec301cf5692778780c9
Yana <example@gmail.com>
2024-12-22 09:31:52 +0200

    menu

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import './styles.modules.scss';
+import Logo from '@images/logo.svg';
+import Image from 'next/image';
 
 const Header: React.FC = () => {
-  return <header>Header</header>;
+  return <header>
+      <div className="container">
+        <div className="logo">
+        <Image
+          src={Logo}
+          alt="Main Banner"
+          width={62}
+          objectFit="cover"
+        />
+        </div>
+      </div>
+    </header>;
 };
 
 export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ import MainBannerImg from '@images/main-banner.jpg';
 import Logo from '@images/logo.svg';
 import Image from 'next/image';
 import { Button } from '@components/Button';
+import vegetarianImg from '@images/vegetarian.png';
+import leaves from '@images/leaves.png';
+import bludo from '@images/bludo.jpg';
 
 const Home = () => {
   return (
@@ -38,7 +41,124 @@ const Home = () => {
         </div>
       </div>
       {/* <TestComponent /> */}
-      <div>Main Page Of the template</div>
+      <div> 
+        <section className="menu">
+          <div className="content">
+            <div className="food-marks">
+              <div className="m-item">
+                <div className="m-ico">
+                <Image
+                  src={vegetarianImg}
+                  alt="Main Banner"
+                  width={25}
+                  objectFit="cover"
+                /></div>
+                <p>Vegetarian dish</p>
+                </div>
+              <div className="m-item">
+                <div className="m-ico">
+                <Image
+                  src={leaves}
+                  alt="Main Banner"
+                  width={25}
+                  objectFit="cover"
+                />
+                </div>
+                <p>Vegan dish</p>
+                </div>
+              <div className="m-item"><p>Your choice</p></div>
+            </div>
+            <h2>Menu</h2>
+            <div className="menu-list">
+              <div className="menu-list-content">
+                <h3>For a Good Beginning</h3>
+                <div className="menu-list__item">
+                  <div style={{ width: '205px' }}>
+                    <Image
+                      src={bludo}
+                      alt="Main Banner"
+                      height={180}
+                      style={{
+                        width: '100%',
+                        borderRadius: '20px',
+                        objectFit: 'cover',
+                      }}
+                    />
+                  </div>
+                  <div className="menu-list-item-descr">
+                    <h4 className="title">Creole potatoes</h4>
+                    <p className="components">
+                    boletus mushrooms/ seasonal vegetables/ cherry tomatoes/ dill sauce</p>
+                  </div>
+                </div>
+
+                <div className="menu-list__item">
+                  <div style={{ width: '205px' }}>
+                    <Image
+                      src={bludo}
+                      alt="Main Banner"
+                      height={180}
+                      style={{
+                        width: '100%',
+                        borderRadius: '20px',
+                        objectFit: 'cover',
+                      }}
+                    />
+                  </div>
+                  <div className="menu-list-item-descr">
+                    <h4 className="title">Creole potatoes</h4>
+                    <p className="components">
+                    boletus mushrooms/ seasonal vegetables/ cherry tomatoes/ dill sauce</p>
+                  </div>
+                </div>
+
+                <div className="menu-list__item">
+                  <div style={{ width: '205px' }}>
+                    <Image
+                      src={bludo}
+                      alt="Main Banner"
+                      height={180}
+                      style={{
+                        width: '100%',
+                        borderRadius: '20px',
+                        objectFit: 'cover',
+                      }}
+                    />
+                  </div>
+                  <div className="menu-list-item-descr">
+                    <h4 className="title">Creole potatoes</h4>
+                    <p className="components">
+                    boletus mushrooms/ seasonal vegetables/ cherry tomatoes/ dill sauce</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="menu-list-content">
+                <h3>For a Good Beginning</h3>
+                <div className="menu-list__item">
+                  <div style={{ width: '205px' }}>
+                    <Image
+                      src={bludo}
+                      alt="Main Banner"
+                      height={180}
+                      style={{
+                        width: '100%',
+                        borderRadius: '20px',
+                        objectFit: 'cover',
+                      }}
+                    />
+                  </div>
+                  <div className="menu-list-item-descr">
+                    <h4 className="title">Creole potatoes</h4>
+                    <p className="components">
+                    boletus mushrooms/ seasonal vegetables/ cherry tomatoes/ dill sauce</p>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </section></div>
     </div>
   );
 };
